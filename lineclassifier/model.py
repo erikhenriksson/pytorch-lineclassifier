fe78af2712532e3be7484193157727a1956db957
Erik <eekihenriksson@gmail.com>
2024-03-13 13:02:49 +0200

    Update model.py

--- lineclassifier/model.py
+++ lineclassifier/model.py
@@ -97,13 +97,13 @@ class XLMRobertaForLineClassification(XLMRobertaPreTrainedModel):
                 start_idx = line_indices[j].item() + 1  # Skip the <s> token
                 if j + 1 == len(line_indices):
                     end_idx = seq_length - 1
+                    print(start_idx, end_idx)
                 else:
                     end_idx = line_indices[j + 1].item()
                 line_repr = sequence_output[i, start_idx:end_idx, :]
 
                 line_repr = torch.mean(line_repr, dim=0, keepdim=True)
                 lines_pooled.append(line_repr)
-                print(lines_pooled)
 
             # lines = sequence_output[i, line_indices, :]
             lines = torch.cat(lines_pooled, dim=0)
